f7d2f18cf1b549ef0b90627159663181b651e75e
Artemiss1509 <stanza-grout54@icloud.com>
2026-05-14 20:18:30 +0530

    Added bootstrap to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,18 @@
-import { Button } from "react-bootstrap";
+import { Button, Navbar, Container } from "react-bootstrap";
 
-const Header = ()=>{
-    return(
-        <>
-        <div>
-            <h1>React Basics</h1>
-            <Button>Users</Button>
-            <Button>Forms</Button>
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" className="px-4 py-3">
+      <Container className="d-flex justify-content-between align-items-center">
+        <h2 className="text-white m-0">React Basics</h2>
+
+        <div className="d-flex gap-2">
+          <Button variant="outline-light">Users</Button>
+          <Button variant="outline-light">Forms</Button>
         </div>
-        </>
-    )
-}
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
